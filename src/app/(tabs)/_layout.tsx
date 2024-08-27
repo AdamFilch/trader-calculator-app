15c469a3a5ac359138244f14d72973908b553b9b
Adam Filchoir <filchoiradam@gmail.com>
2024-08-27 21:36:46 +0700

    add animated tabs to application

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,44 +1,45 @@
-import { Tabs } from "expo-router";
 import React from "react";
 
-import { TabBarIcon } from "@/src/components/navigation/TabBarIcon";
-import { Colors } from "@/src/constants/Colors";
-import { useColorScheme } from "@/src/hooks/useColorScheme";
+import { AnimatedTabBarNavigator } from "react-native-animated-nav-tab-bar";
+import { HouseIcon, Search, Settings2Icon } from "lucide-react-native";
+import HomeScreen from ".";
+import TabTwoScreen from "./explore";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+  const Tabs = AnimatedTabBarNavigator();
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-        headerShown: false,
+    <Tabs.Navigator
+      tabBarOptions={{
+        activeTintColor: "#000000",
+        inactiveTintColor: "#FFF",
       }}
+      appearance={{ tabBarBackground: "black" }}
     >
       <Tabs.Screen
         name="index"
+        component={HomeScreen}
         options={{
           title: "Home",
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon
-              name={focused ? "home" : "home-outline"}
-              color={color}
-            />
-          ),
+          tabBarIcon: ({ color, focused }) => <HouseIcon color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        component={Search}
+        options={{
+          title: "search",
+          tabBarIcon: ({ color, focused }) => <Search color={color} />,
         }}
       />
       <Tabs.Screen
         name="explore"
+        component={TabTwoScreen}
         options={{
-          title: "Explore",
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon
-              name={focused ? "code-slash" : "code-slash-outline"}
-              color={color}
-            />
-          ),
+          title: "Settings",
+          tabBarIcon: ({ color, focused }) => <Settings2Icon color={color} />,
         }}
       />
-    </Tabs>
+    </Tabs.Navigator>
   );
 }
