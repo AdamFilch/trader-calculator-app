fae5aa70fe4a6247fcaddfc85e9eb7ca46bd8288
Adam Filchoir <filchoiradam@gmail.com>
2024-10-08 20:23:42 +0700

    Created first listener

--- backend/server/main.go
+++ backend/server/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	h "stock-trader-server/backend/server/handles"
+	"sync"
+)
+
+type Post struct {
+	ID   int    `json: "id"`
+	Body string `json: "body"`
+}
+
+var (
+	posts   = make(map[int]Post)
+	nextID  = 1
+	postsMu sync.Mutex
+)
 
 func main() {
-    fmt.Println("Hello World")
+	http.HandleFunc("api/h/get-trade-periods", h.HandleGetTradePeriods)
+
+	fmt.Println("Server is running at http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
